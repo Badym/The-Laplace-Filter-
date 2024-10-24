94632827c2ce2eac17ea26381108a44e2e74a269
BlaSz <114978807+Badym@users.noreply.github.com>
2024-10-24 12:49:29 +0200

    Firth commit - created project

---
+++ LapaceFiltration/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaplaceFilter
+{
+    internal static class Program
+    {
+
+        [DllImport("C:\\Users\\badim\\Desktop\\lapaceFiltration\\LapaceFiltration\\x64\\Debug\\JAAsm.dll")]
+
+        static extern int MyProc1(int a, int b);
+
+        [DllImport("C:\\Users\\badim\\Desktop\\lapaceFiltration\\LapaceFiltration\\x64\\Debug\\JAcpp.dll")]
+        static extern int adding(int a, int b);
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+         
+        static void Main()
+        {
+
+
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            Console.Write("Moja pierwsza wartość obliczona w asm to:");
+            Console.WriteLine(retVal);
+            Console.ReadLine();
+            int rer = adding(x, y);
+            Console.Write("Moja pierwsza wartość obliczona w c++ to:");
+            Console.WriteLine(rer);
+            Console.ReadLine();
+        }
+    }
+}
